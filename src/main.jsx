e5ff72d1993bcdcf8d8384a1e4a62eeed068c3a6
Sayem <mustakhasansayem@gmail.com>
2024-05-10 20:39:37 +0600

    Project Set-up done

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <h1 className='font-lato text-3xl'>Hello</h1>
+  </React.StrictMode>,
+)
