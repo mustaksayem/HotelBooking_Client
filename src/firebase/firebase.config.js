ae9c20c44733164b2bbb814a4518c8216cb72543
Sayem <mustakhasansayem@gmail.com>
2024-05-11 03:26:34 +0600

    Login reg Done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8juvb20EPNQpP6dQnwREWDAmxJsN876w",
+  authDomain: "assignment-11-b42b6.firebaseapp.com",
+  projectId: "assignment-11-b42b6",
+  storageBucket: "assignment-11-b42b6.appspot.com",
+  messagingSenderId: "901991724215",
+  appId: "1:901991724215:web:bb9b87329425226f3e0932"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes'
+import AuthProvider from './provider/AuthProvider'
+import {Toaster} from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <h1 className='font-lato text-3xl'>Hello</h1>
+ <AuthProvider>
+ <RouterProvider router={router}></RouterProvider>
+ <Toaster></Toaster>
+ </AuthProvider>
   </React.StrictMode>,
 )
